94db5989b4239be71b2750d7a7975a3dc78af41e
Aman Agarwal <aman.agarwal@IM9758-X0.local>
2023-08-01 20:37:46 +0530

    [Removed] removed the banners concept from advertiser side and put the values in Ad

--- src/main/java/com/develop/adservingservice/Repository/BannerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.develop.adservingservice.Repository;
-
-import com.develop.adservingservice.entity.BannerEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BannerRepository extends CrudRepository<BannerEntity, Long> {
-}
