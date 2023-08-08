985a8c79a52265d7489aa14e281f3ba8ba405a6f
chnd777 <chandumalla035@gmail.com>
2023-08-08 14:34:36 +0530

    prometheus and swagger setup done

---
+++ SwaggerConfig.java
@@ -0,0 +1,32 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2 // Enable Swagger 2. Use @EnableSwagger2WebMvc for newer versions of Springfox.
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.develop.adservingservice.controller")) // Replace with your controller package
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("My API Documentation")
+                .description("Documentation for my Spring Boot API")
+                .version("1.0.0")
+                .build();
+    }
+}
